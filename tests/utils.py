66909d0bc75368a57d57426b2c659aeb50d9987a
Mccallke0364 <114609718+Mccallke0364@users.noreply.github.com>
2024-04-17 12:28:33 -0500

    Update utils.py

--- tests/utils.py
+++ tests/utils.py
@@ -3,4 +3,7 @@ import numpy as np
 
 convert= pd.read_csv("gene_id_to_symbol.csv")
 def id_converter(ids):
+    for id in convert.itertuples():
+        if id[1]==ids:
+            dict[id[1]]=id[3]
   
